22f647a2f5568755409ed3c145ac3a53fcdc02cc
Wesley Cheek <wesley.cheek83@gmail.com>
2024-06-25 20:20:14 +0900

    first commit

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,19 @@
+use std::fmt;
+
+pub struct Clock;
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        todo!("Add {minutes} minutes to existing Clock time");
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "")
+    }
+}
