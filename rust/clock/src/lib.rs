aa8978de01869390323407923a1f0a1f91fc6088
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-07-16 17:14:25 +0900

    Hours rolling over!

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,10 +1,18 @@
-use std::fmt::{self, Display};
+use std::fmt::{self, Debug, Display};
 
-pub struct Clock;
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+    pub fn new(hours: i32, minutes: i32) -> Clock {
+        let formatted_hours: i32 = if hours >= 24 { hours % 24 } else { hours };
+        let formatted_minutes: i32 = if minutes >= 60 { minutes % 60 } else { minutes };
+        Clock {
+            hours: formatted_hours,
+            minutes: formatted_minutes,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
@@ -14,6 +22,18 @@ impl Clock {
 
 impl Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
+    }
+}
+
+impl Debug for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "")
+    }
+}
+
+impl PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        (self.hours == other.hours) && (self.minutes == other.minutes)
     }
 }
--- rust/clock/tests/clock.rs
+++ rust/clock/tests/clock.rs
@@ -10,25 +10,21 @@ fn on_the_hour() {
 }
 
 #[test]
-#[ignore]
 fn past_the_hour() {
     assert_eq!(Clock::new(11, 9).to_string(), "11:09");
 }
 
 #[test]
-#[ignore]
 fn midnight_is_zero_hours() {
     assert_eq!(Clock::new(24, 0).to_string(), "00:00");
 }
 
 #[test]
-#[ignore]
 fn hour_rolls_over() {
     assert_eq!(Clock::new(25, 0).to_string(), "01:00");
 }
 
 #[test]
-#[ignore]
 fn hour_rolls_over_continuously() {
     assert_eq!(Clock::new(100, 0).to_string(), "04:00");
 }
