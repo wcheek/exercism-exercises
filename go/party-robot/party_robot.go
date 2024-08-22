4ee9dd345b494f7d283b9c3476dfdc7d38da5113
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-08-22 09:00:11 +0900

    Working on Go path

---
+++ go/party-robot/party_robot.go
@@ -0,0 +1,21 @@
+package partyrobot
+
+import "fmt"
+
+// Welcome greets a person by name.
+func Welcome(name string) string {
+	return fmt.Sprintf("Welcome to my party, %s!", name)
+}
+
+// HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
+func HappyBirthday(name string, age int) string {
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
+}
+
+// AssignTable assigns a table to each guest.
+func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
+	const myString = "%s" + "\n" +
+		"You have been assigned to table %.3d. Your table is %s, exactly %.1f meters from here." + "\n" +
+		"You will be sitting next to %s."
+	return fmt.Sprintf(myString, Welcome(name), table, direction, distance, neighbor)
+}
